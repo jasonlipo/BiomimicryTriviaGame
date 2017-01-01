bb2e100a14c5195ac6e3f6745b42f9e6a611f3f4
Boaz Francis <Boaz Francis>
2017-01-01 23:22:23 +0000

    Added Questions to TriviaGame

--- www/Trivia/client.js
+++ www/Trivia/client.js
@@ -6,55 +6,55 @@ $(function () {
 		{
 			"question": "מה ההבדל בין הנדסה גנטית לביומימקרי?",
 			"answers": [
-				{ "text": "Answer 1 is incorrect", "correct": false },
-				{ "text": "Answer 2 is incorrect", "correct": false },
+				{ "text": "ביו-מימקרי עוסק בשינוי גנים של בעלי חיים ואילו הנדסה גנטית עוסקת בשינוי הגנים של צמחים.", "correct": false },
+				{ "text": "ביו-מימקרי הוא שמו המדעי של תחום ההנדסה הגנטית.", "correct": false },
 				{ "text": "ביו-מימקרי מחפש השראה ורעיונות בטבע בעוד הנדסה גנטית משנה את הטבע על ידי התערבות במבנה הגנטי של צמחים ושל בעלי חיים.", "correct": true },
-				{ "text": "Answer 4 is incorrect", "correct": false }
+				{ "text": "ביו-מימקרי משתמש בכלים מן הטבע כדי לתכנן גנים חדשים בעוד הנדסה גנטית משנה את הטבע על ידי התערבות במבנה הגנטי של צמחים.", "correct": false }
 			]
 		},
 		{
-			"question": "This is question 2.",
+			"question": "האם מדע הביומימיקרי הינו המצאה חדשה?",
 			"answers": [
-				{ "text": "Answer 1 is correct", "correct": true },
-				{ "text": "Answer 2 is incorrect", "correct": false },
-				{ "text": "Answer 3 is incorrect", "correct": false },
-				{ "text": "Answer 4 is incorrect", "correct": false }
+				{ "text":".מדע הביומימקרי הינו טכניקה שהשתמשו בה כבר במאה ה-16", "correct": true },
+				{ "text": "מדע הביומימקרי הינו מדע חדש שנוסד על ידי חברות הייטק.", "correct": false },
+				{ "text": "הביומימקרי הומצא במאה ה-19 במהפכה התעשייתית.", "correct": false },
+				{ "text": "אין לדעת מתי הומצא מדע הביומימקרי.", "correct": false }
 			]
 		},
 		{
-			"question": "This is question 2.",
+			"question": "באיזו שיטה משתמשים בביומימיקרי כדי לתכנן מוצרים המבוססים על הטבע?",
 			"answers": [
-				{ "text": "Answer 1 is correct", "correct": true },
-				{ "text": "Answer 2 is incorrect", "correct": false },
-				{ "text": "Answer 3 is incorrect", "correct": false },
-				{ "text": "Answer 4 is incorrect", "correct": false }
+				{ "text": "אין שיטה מיוחדת", "correct": false },
+				{ "text": "חשיבת תכנון (Design thinking)", "correct": true },
+				{ "text": "רישום פטנטים", "correct": false },
+				{ "text": "חשיבה מסועפת ומתכנסת ", "correct": false }
 			]
 		},
 		{
-			"question": "This is question 2.",
+			"question": "מה היה הפיתוח הראשון בהשראת ביומימיקרי בשנת 1505?",
 			"answers": [
-				{ "text": "Answer 1 is correct", "correct": true },
-				{ "text": "Answer 2 is incorrect", "correct": false },
-				{ "text": "Answer 3 is incorrect", "correct": false },
-				{ "text": "Answer 4 is incorrect", "correct": false }
+				{ "text": "המצאת נורת החשמל", "correct": false },
+				{ "text": "המצאת ההליקופטר", "correct": true },
+				{ "text": "המצאת הטלפון", "correct": false },
+				{ "text": "המצאת המכונית", "correct": false }
 			]
 		},
 		{
-			"question": "This is question 2.",
+			"question": "מי היה המדען הראשון שידוע שהשתמש בביומימקרי  להמצאותיו?",
 			"answers": [
-				{ "text": "Answer 1 is correct", "correct": true },
-				{ "text": "Answer 2 is incorrect", "correct": false },
-				{ "text": "Answer 3 is incorrect", "correct": false },
-				{ "text": "Answer 4 is incorrect", "correct": false }
+				{ "text": "אלכסנדר בל", "correct": false },
+				{ "text": "מיכלאנג'לו", "correct": false },
+				{ "text": "תומס אלווה אדיסון", "correct": false },
+				{ "text": "ליאונרדו דה-וינצ'י", "correct": true }
 			]
 		},
 		{
-			"question": "This is question 2.",
+			"question": "מהו כלי המחקר שבו משתמשים כדי ללמוד מהטבע?",
 			"answers": [
-				{ "text": "Answer 1 is correct", "correct": true },
-				{ "text": "Answer 2 is incorrect", "correct": false },
-				{ "text": "Answer 3 is incorrect", "correct": false },
-				{ "text": "Answer 4 is incorrect", "correct": false }
+				{ "text": "תצפית", "correct": true },
+				{ "text": "שאלונים", "correct": false },
+				{ "text": "ראיונות עם אנשים", "correct": false },
+				{ "text": "ויקיפדיה", "correct": false }
 			]
 		}
 	]);
