2e0641acbd906441332d7eb0172c3ffe217f8ed7
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-02 15:37:15 +0000

    Added highscore table to Trivia game

---
+++ www/files.js
@@ -0,0 +1,34 @@
+var fh = {};
+
+function gotFS(fs) {
+	fs.root.getFile(FILENAME, {create: true, exclusive: false}, gotFileEntry, null);
+}
+function gotFileEntry(fileEntry) {
+	fh.entry = fileEntry;
+	fileEntry.createWriter(gotFileWriter, null);
+}
+function gotFileWriter(fileWriter) {
+	fh.writer = fileWriter;
+}
+function saveText(val) {
+	fh.writer.onwriteend = function (evt) {
+		fh.writer.seek(0);
+	}
+	fh.writer.write(val);
+}
+function readText(callback) {
+	if (fh.entry) {
+		fh.entry.file(function (file) {
+			var reader = new FileReader();
+			reader.onloadend = function (evt) {
+				t = evt.target.result;
+				callback(t);
+			}
+			reader.readAsText(file);
+		}, null);
+	}
+	return false;
+}
+function clearText() {
+	fh.writer.truncate(0);
+}
