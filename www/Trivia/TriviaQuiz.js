9b536c895524bac432247e2891442394d74e4d2a
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-01-02 15:37:15 +0000

    Added highscore table to Trivia game

--- www/Trivia/TriviaQuiz.js
+++ www/Trivia/TriviaQuiz.js
@@ -1,3 +1,8 @@
+var FILENAME = 'TriviaHighScores.txt';
+document.addEventListener('deviceready', function () {
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, null);
+}, false);
+
 var TriviaQuiz = function(t, x) {
 
     // TriviaQuiz.initialise
@@ -7,11 +12,25 @@ var TriviaQuiz = function(t, x) {
         this.target = t;
         this.question_data = x;
         this.current_question = -1;
-        this.score = 0;
+        this.score = 0;       
+
+        this.shuffle(this.question_data);
         
         // Start the first question
         this.nextQuestion();
 
+    }
+
+    // TriviaQuiz.shuffle
+    // Randomise and array
+    this.shuffle = function(a) {
+        var j, x, i;
+        for (i = a.length; i; i--) {
+            j = Math.floor(Math.random() * i);
+            x = a[i - 1];
+            a[i - 1] = a[j];
+            a[j] = x;
+        }
     }
 
     // TriviaQuiz.nextQuestion
@@ -31,6 +50,8 @@ var TriviaQuiz = function(t, x) {
         var this_question = this_obj["question"];
         var this_answers = this_obj["answers"];
         
+        this.shuffle(this_answers);
+
         // Fill the DOM with the current question and answers
         this.target.find('.question').html((this.current_question+1) + ". " + this_question);
         for (var i=0; i<this_answers.length; i++) {
@@ -104,9 +125,55 @@ var TriviaQuiz = function(t, x) {
     // TriviaQuiz.endGame
     // Finish the game and show highscore table
     this.endGame = function () {
-        alert("You got " + this.score + " out of " + this.question_data.length + " questions correct.\n" +
-            "Thanks for playing");
-        this.target.html('');
+        readText(function (text) {
+            scores = JSON.parse(text);
+            scores.sort(function (a, b) {
+                return b.score - a.score;
+            });
+            $('.question').html('Highscore Leaderboard');
+            $('.answers').after('<div class="scores"></div>');
+            $('.answers').hide();
+            numberToDisplay = 5;
+            if (scores.length >= numberToDisplay) {
+                if (this.score > scores[numberToDisplay-1].score) {
+                    rowsToDisplay = numberToDisplay-1;
+                }
+                else {
+                    rowsToDisplay = numberToDisplay;
+                }
+            }
+            else {
+                rowsToDisplay = scores.length;
+            }
+            positionedScore = false;
+            for (i=0; i<rowsToDisplay; i++) {
+                if (this.score > scores[i].score && !positionedScore) {
+                    positionedScore = true;
+                    $('.scores').append('<div class="row s"><div class="name"><input type="text" class="highscore-name" /></div><div class="number">'+this.score+'</div></div>');
+                }
+                $('.scores').append('<div class="row"><div class="name">'+scores[i].name+'</div><div class="number">'+scores[i].score+'</div></div>');
+            }
+            if (scores.length < numberToDisplay) {
+                $('.scores').append('<div class="row s"><div class="name"><input type="text" class="highscore-name" /></div><div class="number">'+this.score+'</div></div>');
+            }
+            $('.scores').append('<br /><button class="finish">Finish</button>');
+            $('.finish').click(function () {
+                name = $('.highscore-name').val();
+                new_scores = scores.splice(0, numberToDisplay - 1);
+                new_scores.push({
+                    name: name,
+                    score: this.score
+                })
+                saveText(JSON.stringify(new_scores));
+                this.reset();
+            }.bind(this));
+        }.bind(this));
+    }
+
+    // TriviaQuiz.reset
+    // Re-start the game
+    this.reset = function () {
+        location.href='/';
     }
 
     this.initialise(t, x);
